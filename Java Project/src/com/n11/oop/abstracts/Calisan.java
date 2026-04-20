f9498b9e8267a3615131f048ae865c3e8eb95e10
Yigit Turan <yit2@calvin.edu>
2026-04-20 19:49:28 +0300

    Ders 2.2(18.04.2026) - diziler ve overload method ornek

--- Java Project/src/com/n11/oop/abstracts/Calisan.java
+++ Java Project/src/com/n11/oop/abstracts/Calisan.java
@@ -3,6 +3,9 @@ package com.n11.oop.abstracts;
 public abstract class Calisan {
      String Pozisyon;
      int Maas;
+     public String ad;
+     public String soyad;
+     public String Departman;
 
      abstract void PozisyonMaas();
 
